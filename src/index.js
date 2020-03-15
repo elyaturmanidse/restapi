946ba6c3b2a4f561322dd815d07deba175b07491
Эльвира Турманидзе <elviraturmanidze@Elya.local>
2020-03-15 14:24:09 +0100

    added 404 and 500 error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 let express = require("express")
 let app = express()
 let personRoute = require('./routes/person')
+let path = require('path')
+
+//Middleware function aka pipeline 
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+    next()
+})
 
 app.use(personRoute)
 app.use(express.static('public'))
+
+//404 handler 
+app.use((req, res, next) => {
+    res.status(404).send('broken')
+})
+
+//500 handler 
+app.use((err, req, res, next) =>{
+    console.log(err.stack);
+    res.sendFile(path.join(__dirname, '../public/500.html'))
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`))
--- src/routes/person.js
+++ src/routes/person.js
@@ -1,7 +1,22 @@
 let express = require('express')
 let router = express.Router()
+
+//part of the query string 
 router.get('/person', (req, res) => {
-    res.send('person requested')
+    if(req.query.name) {
+        res.send(`person requested ${req.query.name}`) //can be called like person?name=thomas^age=19
+    }else{
+        res.send('person requested')
+    }
+})
+
+//Params property on teh request object == part of the route 
+router.get('/person/:name', (req, res) => {
+    res.send(`person requested ${req.params.name}`) //can do person/thomas
+})
+
+router.get('/error', (req, res) =>{
+    throw new Error('error') //triggers exceprion that gets handeled in index.js
 })
 
 module.exports = router 
